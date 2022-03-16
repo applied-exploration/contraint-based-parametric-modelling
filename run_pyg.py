0d8d956aa74530b238f00397c6cb023edf42d330
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-03-16 12:59:30 +0100

    feat(Dependencies): added environment.yml (works on m1)

---
+++ run_pyg.py
@@ -0,0 +1,12 @@
+from sketchgraphs_models.graph.train.data_loading import load_dataset_and_weights
+
+
+dataset_train_path = 'sg_t16_train.npy'
+quantization = {'angle': 127, 'length': 383}
+seed = 7
+from torch_geometric.data import Data
+
+ds_train, weightsgr_train = load_dataset_and_weights(dataset_train_path, None, quantization, seed, True, True, False)
+
+first_g = next(iter(ds_train))[0]
+data = Data(first_g.node_features, first_g.incidence)
