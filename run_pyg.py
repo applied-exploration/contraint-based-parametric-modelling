ae3135e3153cf36ab8c744fb19d603a7c3501b7b
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-03-16 15:48:51 +0100

    fix(Dependencies): added tensorboard

--- run_pyg.py
+++ run_pyg.py
@@ -6,7 +6,7 @@ quantization = {'angle': 127, 'length': 383}
 seed = 7
 from torch_geometric.data import Data
 
-ds_train, weightsgr_train = load_dataset_and_weights(dataset_train_path, None, quantization, seed, True, True, False)
+ds_train, weightsgr_train = load_dataset_and_weights(dataset_train_path, None, quantization, seed, entity_features=False, edge_features=False, force_entity_categorical_features=False)
 
 first_g = next(iter(ds_train))[0]
 data = Data(first_g.node_features, first_g.incidence)
