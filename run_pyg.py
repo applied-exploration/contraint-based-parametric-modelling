bd063f403ffd664690fcc9b948843b0a0a92c7d0
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2022-03-16 18:00:13 +0100

    fix(Dataset): rename function

--- run_pyg.py
+++ run_pyg.py
@@ -5,7 +5,7 @@ quantization = {'angle': 127, 'length': 383}
 seed = 7
 
 
-ds_train, weightsgr_train = load_homogeneous_dataset(dataset_train_path, None, quantization, seed, entity_features=False, edge_features=False, force_entity_categorical_features=False)
+ds_train, weightsgr_train = load_homogeneous_dataset(dataset_train_path, None, quantization, seed, force_entity_categorical_features=False)
 
 
 ds_train
