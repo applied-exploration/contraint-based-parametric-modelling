cc326f4079954e812ef052a32b06ef6ec17226bd
Daniel Szemerey <daniel.szemerey.15@ucl.ac.uk>
2022-03-16 20:14:03 +0100

    feat: Added data metric.

--- run_pyg.py
+++ run_pyg.py
@@ -1,6 +1,6 @@
 from pyg_homo_dataset import load_homogeneous_dataset
 
-dataset_train_path = 'sg_t16_train.npy'
+dataset_train_path = 'sequence_data/sg_t16_train.npy'
 quantization = {'angle': 127, 'length': 383}
 seed = 7
 
@@ -8,4 +8,15 @@ seed = 7
 ds_train, weightsgr_train = load_homogeneous_dataset(dataset_train_path, None, quantization, seed, force_entity_categorical_features=False)
 
 
-ds_train
+
+def get_parameters(data):
+    print("== Data Metrics ==")
+    print(f"num_nodes: {data.num_nodes}")
+    print(f"num_edges: {data.num_edges}")
+    print(f"num_node_features: {data.num_node_features}")
+    print(f"Is the graph directed?: {data.is_directed()}")
+    print(f"Self loops in graph?: {data.has_self_loops()}")
+    print(f"Any isolated nodes?: {data.has_isolated_nodes()}")
+
+single_example = ds_train.__getitem__(0)[0]
+get_parameters(single_example)
